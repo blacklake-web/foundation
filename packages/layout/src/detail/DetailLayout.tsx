347d4abc43fed8c7aa30c6304acdf6f9a4773cf2
Qixiaona0908 <1051913744@qq.com>
2021-10-12 15:31:58 +0800

    feat(样式调整):  表单样式修改

--- packages/layout/src/detail/DetailLayout.tsx
+++ packages/layout/src/detail/DetailLayout.tsx
@@ -4,16 +4,13 @@ import { DetailLayoutContent } from './components/DetailLayoutContent';
 import { DetailLayoutTitle } from './components/DetailLayoutTitle';
 //
 import { DetailLayoutProps } from './DetailLayout.type';
-
-const detailContextStyle = {
-  display: 'flex',
-};
+import './components/DetailLayoutContent.less';
 
 const BlDetailLayout = (props: DetailLayoutProps) => {
   const { title, info, extra, baseMenu, dataSource, children } = props;
 
   return (
-    <div style={{ ...detailContextStyle, flexDirection: 'column' }}>
+    <div className="detail-layout">
       <DetailLayoutTitle title={title} extra={extra} baseMenu={baseMenu} />
       <DetailLayoutContent info={info} dataSource={dataSource} />
       {children}
